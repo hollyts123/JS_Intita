333c2cb40f91dfb59255e89e45c328efbd4b6527
Tatiana Slonovskaya <tatianaslonovskaya@gmail.com>
2023-11-24 00:21:58 +0100

    solved problem 5.6_2

---
+++ task5.6_2/script.js
@@ -0,0 +1,12 @@
+function generateRandomNum() {
+   const randomFunction = function () {
+      return console.log(Math.trunc(Math.random() * 1000));
+   }
+   randomFunction();
+}
+
+generateRandomNum()
+generateRandomNum()
+generateRandomNum()
+generateRandomNum()
+generateRandomNum()
