ca4dfdd7ea299792d1759564906abfb1738abe11
Tatiana Slonovskaya <tatianaslonovskaya@gmail.com>
2023-11-24 00:03:16 +0100

    solved problem 5.6_1

---
+++ task5.6_1/script.js
@@ -0,0 +1,10 @@
+function addNumbers(num1 = 0, num2 = 0) {
+   let sum = num1 + num2;
+   console.log(sum);
+   return sum;
+}
+
+addNumbers(5, 3);
+addNumbers(16);
+addNumbers(1981, 576);
+addNumbers(null, 93);
